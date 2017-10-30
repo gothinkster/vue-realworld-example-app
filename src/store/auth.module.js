abb06acf5d4437982ec85014ab2d4976aefffaa0
disjfa <disjfa@disjfa.nl>
2017-10-30 21:49:16 +0000

    Started fixing settings page
    Added route after saving
    Added put to apiclient
    Setup babelrc for building
    Added gh-pages for deployment to gh-pages

--- src/store/auth.module.js
+++ src/store/auth.module.js
@@ -1,24 +1,33 @@
 import ApiService from '@/common/api.service'
 import JwtService from '@/common/jwt.service'
-import { LOGIN, LOGOUT, REGISTER, CHECK_AUTH } from './actions.type'
+import { LOGIN, LOGOUT, REGISTER, CHECK_AUTH, UPDATE_USER } from './actions.type'
 import { SET_AUTH, PURGE_AUTH, SET_ERROR } from './mutations.type'
 
-export const state = {
+const state = {
   errors: null,
   user: {},
   isAuthenticated: !!JwtService.getToken()
 }
 
-export const actions = {
+const getters = {
+  getUser (state) {
+    return state.user
+  },
+  isAuthenticated (state) {
+    return state.isAuthenticated
+  }
+}
+
+const actions = {
   [LOGIN] (context, credentials) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       ApiService
-        .post('users/login', { user: credentials })
-        .then(({ data }) => {
+        .post('users/login', {user: credentials})
+        .then(({data}) => {
           context.commit(SET_AUTH, data.user)
           resolve(data)
         })
-        .catch(({ response }) => {
+        .catch(({response}) => {
           context.commit(SET_ERROR, response.data.errors)
         })
     })
@@ -29,12 +38,12 @@ export const actions = {
   [REGISTER] (context, credentials) {
     return new Promise((resolve, reject) => {
       ApiService
-        .post('users', { user: credentials })
-        .then(({ data }) => {
+        .post('users', {user: credentials})
+        .then(({data}) => {
           context.commit(SET_AUTH, data.user)
           resolve(data)
         })
-        .catch(({ response }) => {
+        .catch(({response}) => {
           context.commit(SET_ERROR, response.data.errors)
         })
     })
@@ -44,19 +53,38 @@ export const actions = {
       ApiService.setHeader()
       ApiService
         .get('user')
-        .then(({ data }) => {
+        .then(({data}) => {
           context.commit(SET_AUTH, data.user)
         })
-        .catch(({ response }) => {
+        .catch(({response}) => {
           context.commit(SET_ERROR, response.data.errors)
         })
     } else {
       context.commit(PURGE_AUTH)
     }
+  },
+  [UPDATE_USER] (context, payload) {
+    const {email, username, password, image, bio} = payload
+    const user = {
+      email,
+      username,
+      bio,
+      image
+    }
+    if (password) {
+      user.password = password
+    }
+
+    return ApiService
+      .put('user', user)
+      .then(({data}) => {
+        context.commit(SET_AUTH, data.user)
+        return data
+      })
   }
 }
 
-export const mutations = {
+const mutations = {
   [SET_ERROR] (state, error) {
     state.errors = error
   },
@@ -77,5 +105,6 @@ export const mutations = {
 export default {
   state,
   actions,
-  mutations
+  mutations,
+  getters
 }
