12837248ef158cc1f1e4e762f117932d33395327
disjfa <disjfa@disjfa.nl>
2017-11-01 20:23:41 +0000

    Setup html for 2 spaces indentation in my editor

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,50 +1,50 @@
 <template>
-    <div class="settings-page">
-        <div class="container page">
-            <div class="row">
-                <div class="col-md-6 offset-md-3 col-xs-12">
-                    <h1 class="text-xs-center">Your Settings</h1>
-                    <form @submit.prevent="updateSettings()">
-                        <fieldset>
-                            <fieldset class="form-group">
-                                <input class="form-control" type="text"
-                                       v-model="user.image"
-                                       placeholder="URL of profile picture">
-                            </fieldset>
-                            <fieldset class="form-group">
-                                <input class="form-control form-control-lg" type="text"
-                                       v-model="user.username"
-                                       placeholder="Your username">
-                            </fieldset>
-                            <fieldset class="form-group">
-                                <textarea class="form-control form-control-lg" rows="8"
-                                          v-model="user.bio"
-                                          placeholder="Short bio about you"></textarea>
-                            </fieldset>
-                            <fieldset class="form-group">
-                                <input class="form-control form-control-lg" type="text"
-                                       v-model="user.email"
-                                       placeholder="Email">
-                            </fieldset>
-                            <fieldset class="form-group">
-                                <input class="form-control form-control-lg" type="password"
-                                       v-model="user.password"
-                                       placeholder="Password">
-                            </fieldset>
-                            <button class="btn btn-lg btn-primary pull-xs-right">
-                                Update Settings
-                            </button>
-                        </fieldset>
-                    </form>
-                    <!-- Line break for logout button -->
-                    <hr>
-                    <button @click="logout" class="btn btn-outline-danger">
-                        Or click here to logout.
-                    </button>
-                </div>
-            </div>
+  <div class="settings-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Your Settings</h1>
+          <form @submit.prevent="updateSettings()">
+            <fieldset>
+              <fieldset class="form-group">
+                <input class="form-control" type="text"
+                       v-model="user.image"
+                       placeholder="URL of profile picture">
+              </fieldset>
+              <fieldset class="form-group">
+                <input class="form-control form-control-lg" type="text"
+                       v-model="user.username"
+                       placeholder="Your username">
+              </fieldset>
+              <fieldset class="form-group">
+                <textarea class="form-control form-control-lg" rows="8"
+                          v-model="user.bio"
+                          placeholder="Short bio about you"></textarea>
+              </fieldset>
+              <fieldset class="form-group">
+                <input class="form-control form-control-lg" type="text"
+                       v-model="user.email"
+                       placeholder="Email">
+              </fieldset>
+              <fieldset class="form-group">
+                <input class="form-control form-control-lg" type="password"
+                       v-model="user.password"
+                       placeholder="Password">
+              </fieldset>
+              <button class="btn btn-lg btn-primary pull-xs-right">
+                Update Settings
+              </button>
+            </fieldset>
+          </form>
+          <!-- Line break for logout button -->
+          <hr>
+          <button @click="logout" class="btn btn-outline-danger">
+            Or click here to logout.
+          </button>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
   import { LOGOUT, UPDATE_USER } from '@/store/actions.type'
