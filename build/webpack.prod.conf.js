82524e582f1b3526bcce03c9eb00959aa4218fe3
Maurice Williams <morficus@gmail.com>
2017-12-21 11:32:38 -0500

    adding ability to generate favicons
    * new dev dependency: favicons-webpack-plugin
    * configured to pick up the file `static/logo.png`
    * closes #2

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,6 +8,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+var FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -96,7 +97,12 @@ var webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    // generate favicons
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, '../static/logo.png'),
+      prefix: 'favicons-[hash]/'
+    })
   ]
 })
 
