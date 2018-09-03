1557bf7cb40193f36e48afdc8a1573f634e38a3e
Jack Koppa <hello@jackkoppa.com>
2018-09-03 18:57:56 -0400

    Implement initial, working e2e specs Only replace initial 3, default Vue specs Door is now open to implement more comprehensive e2e suite

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,9 +11,8 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.navbar')
+      .assert.containsText('.navbar-brand', 'conduit')
       .end()
   }
 }
