3e739ed641d61e472dc594135e0863d5ff8632fd
matheushf <hffmatheus@gmail.com>
2018-10-12 18:43:36 -0300

    min

--- src/common/jwt.service.js
+++ src/common/jwt.service.js
@@ -1,14 +1,14 @@
 const ID_TOKEN_KEY = "id_token";
 
-const getToken = () => {
+export const getToken = () => {
   return window.localStorage.getItem(ID_TOKEN_KEY);
 };
 
-const saveToken = token => {
+export const saveToken = token => {
   window.localStorage.setItem(ID_TOKEN_KEY, token);
 };
 
-const destroyToken = () => {
+export const destroyToken = () => {
   window.localStorage.removeItem(ID_TOKEN_KEY);
 };
 
