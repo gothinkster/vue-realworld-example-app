63d8467670a183ca447d038b32f20ae8aed05ea5
Matheus Victor <hffmatheus@gmail.com>
2018-10-12 18:46:06 -0300

    Export default jwt.service (#78)
    * shorthands
    
    * default export
    
    * min

--- src/common/jwt.service.js
+++ src/common/jwt.service.js
@@ -1,15 +1,15 @@
 const ID_TOKEN_KEY = "id_token";
 
-export default {
-  getToken() {
-    return window.localStorage.getItem(ID_TOKEN_KEY);
-  },
-
-  saveToken(token) {
-    window.localStorage.setItem(ID_TOKEN_KEY, token);
-  },
-
-  destroyToken() {
-    window.localStorage.removeItem(ID_TOKEN_KEY);
-  }
+export const getToken = () => {
+  return window.localStorage.getItem(ID_TOKEN_KEY);
 };
+
+export const saveToken = token => {
+  window.localStorage.setItem(ID_TOKEN_KEY, token);
+};
+
+export const destroyToken = () => {
+  window.localStorage.removeItem(ID_TOKEN_KEY);
+};
+
+export default { getToken, saveToken, destroyToken };
