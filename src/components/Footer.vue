096a18f17c67e8802f44314fb62a03c980a9991c
Vinay <vinaysagar971995@gmail.com>
2018-10-13 11:14:17 +0530

    renamed rwvFooter to  RwvFooter

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div class="container">
+      <router-link class="logo-font"
+        :to="{ name: 'home', params: {} }">
+        conduit
+      </router-link>
+      <span class="attribution">
+        An interactive learning project from
+        <a target="blank" href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "RwvFooter"
+};
+</script>
