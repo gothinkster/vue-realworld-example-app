dbc7b06ba2aa8196ba1cab1a2c4202ff4e09555b
igeligel <kevinigeligeligel@gmail.com>
2018-10-28 19:40:45 +0100

    inline texts and format code block

--- src/components/VArticlePreview.vue
+++ src/components/VArticlePreview.vue
@@ -6,15 +6,18 @@
     />
     <router-link
       :to="articleLink"
-      class="preview-link">
-      <h1>{{article.title}}</h1>
-      <p>{{article.description}}</p>
+      class="preview-link"
+    >
+      <h1 v-text="article.title" />
+      <p v-text="article.description" />
       <span>Read more...</span>
       <ul class="tag-list">
-        <li class="tag-default tag-pill tag-outline"
-         v-for="(tag, index) of article.tagList"
-         :key="tag + index">
-         <span>{{ tag }}</span>
+        <li
+          class="tag-default tag-pill tag-outline"
+          v-for="(tag, index) of article.tagList"
+          :key="tag + index"
+        >
+          <span v-text="tag" />
         </li>
       </ul>
     </router-link>
