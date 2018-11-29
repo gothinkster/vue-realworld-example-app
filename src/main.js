9f799d5922904314bba8721a377647cb3cf813af
Kevin Peters <kevin.peters@klarna.com>
2018-11-29 11:04:35 +0100

    use immediate return and relative paths

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
-import { CHECK_AUTH } from "@/store/actions.type";
-import ApiService from "@/common/api.service";
-import DateFilter from "@/common/date.filter";
-import ErrorFilter from "@/common/error.filter";
+import { CHECK_AUTH } from "./store/actions.type";
+import ApiService from "./common/api.service";
+import DateFilter from "./common/date.filter";
+import ErrorFilter from "./common/error.filter";
 
 Vue.config.productionTip = false;
 Vue.filter("date", DateFilter);
@@ -16,9 +16,9 @@ Vue.filter("error", ErrorFilter);
 ApiService.init();
 
 // Ensure we checked auth before each page load.
-router.beforeEach((to, from, next) => {
-  return Promise.all([store.dispatch(CHECK_AUTH)]).then(next);
-});
+router.beforeEach((to, from, next) =>
+  Promise.all([store.dispatch(CHECK_AUTH)]).then(next)
+);
 
 new Vue({
   router,
