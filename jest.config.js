9f9a90f354b3c69efa063a32d2ba569775d0077c
Emma Belanger <emmabelanger@Emmas-MacBook-Pro.local>
2019-08-21 19:38:17 -0500

    remove coverage file

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,10 @@ module.exports = {
   moduleFileExtensions: ["js", "jsx", "json", "vue"],
   transform: {
     "^.+\\.vue$": "vue-jest",
-    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
     "^.+\\.(js|jsx)?$": "babel-jest"
   },
-  collectCoverage: true,
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1"
   },
@@ -14,5 +13,5 @@ module.exports = {
   testMatch: [
     "<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))"
   ],
-  transformIgnorePatterns: ["<rootDir>/node_modules/(?!Vue)"]
+  transformIgnorePatterns: ["<rootDir>/node_modules/"]
 };
