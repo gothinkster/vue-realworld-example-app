02488091d6e20c95fd39d0dd68cf9ead607f128b
Simon <simon.kotov.web@gmail.com>
2021-01-13 15:09:53 +0300

    add link for MIT page in Wiki

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -8,7 +8,10 @@
         An interactive learning project from
         <a rel="noopener noreferrer" target="blank" href="https://thinkster.io"
           >Thinkster</a
-        >. Code &amp; design licensed under MIT.
+        >. Code &amp; design licensed under
+        <a target="_blank" href="https://en.wikipedia.org/wiki/MIT_License"
+          >MIT</a
+        >.
       </span>
     </div>
   </footer>
