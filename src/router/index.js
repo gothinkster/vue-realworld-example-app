0db5fc8bd560a95905997257b996e7b39b2b1f9f
khaledosman <khaled.osmaan@gmail.com>
2021-02-13 12:02:02 +0100

    perf: prefetch chunks, preload main chunks, dont lazyload default route

--- src/router/index.js
+++ src/router/index.js
@@ -1,76 +1,80 @@
 import Vue from "vue";
 import Router from "vue-router";
-
+import Home from "@/views/Home";
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: "/",
-      component: () => import("@/views/Home"),
+      component: Home,
       children: [
         {
           path: "",
           name: "home",
-          component: () => import("@/views/HomeGlobal")
+          component: () =>
+            import(/* webpackPreload: true */ "@/views/HomeGlobal")
         },
         {
           path: "my-feed",
           name: "home-my-feed",
-          component: () => import("@/views/HomeMyFeed")
+          component: () =>
+            import(/* webpackPrefetch: true */ "@/views/HomeMyFeed")
         },
         {
           path: "tag/:tag",
           name: "home-tag",
-          component: () => import("@/views/HomeTag")
+          component: () => import(/* webpackPrefetch: true */ "@/views/HomeTag")
         }
       ]
     },
     {
       name: "login",
       path: "/login",
-      component: () => import("@/views/Login")
+      component: () => import(/* webpackPrefetch: true */ "@/views/Login")
     },
     {
       name: "register",
       path: "/register",
-      component: () => import("@/views/Register")
+      component: () => import(/* webpackPrefetch: true */ "@/views/Register")
     },
     {
       name: "settings",
       path: "/settings",
-      component: () => import("@/views/Settings")
+      component: () => import(/* webpackPrefetch: true */ "@/views/Settings")
     },
     // Handle child routes with a default, by giving the name to the
     // child.
     // SO: https://github.com/vuejs/vue-router/issues/777
     {
       path: "/@:username",
-      component: () => import("@/views/Profile"),
+      component: () => import(/* webpackPrefetch: true */ "@/views/Profile"),
       children: [
         {
           path: "",
           name: "profile",
-          component: () => import("@/views/ProfileArticles")
+          component: () =>
+            import(/* webpackPrefetch: true */ "@/views/ProfileArticles")
         },
         {
           name: "profile-favorites",
           path: "favorites",
-          component: () => import("@/views/ProfileFavorited")
+          component: () =>
+            import(/* webpackPrefetch: true */ "@/views/ProfileFavorited")
         }
       ]
     },
     {
       name: "article",
       path: "/articles/:slug",
-      component: () => import("@/views/Article"),
+      component: () => import(/* webpackPrefetch: true */ "@/views/Article"),
       props: true
     },
     {
       name: "article-edit",
       path: "/editor/:slug?",
       props: true,
-      component: () => import("@/views/ArticleEdit")
+      component: () => import(/* webpackPrefetch: true */ "@/views/ArticleEdit")
     }
   ]
 });
