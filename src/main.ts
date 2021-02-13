f5d33378a16cd7e90cd499c73714067face88fe2
khaledosman <khaled.osmaan@gmail.com>
2021-02-13 20:51:26 +0100

    get rid of unused dependency vue-axios & enable bundle analyzer

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-import VueAxios from "vue-axios";
 import axios from "axios";
 
 import { CHECK_AUTH } from "./store/actions.type";
@@ -11,4 +10,4 @@ import { CHECK_AUTH } from "./store/actions.type";
 // Ensure we checked auth before each page load.
 store.dispatch(CHECK_AUTH);
 
-createApp(App).use(router).use(store).use(VueAxios, axios).mount("#app");
+createApp(App).use(router).use(store).mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: false,
+      analyzerPort: "auto"
+    }
+  }
+};
