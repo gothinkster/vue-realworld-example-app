5c381dd6d82f282dd11373da73e0ed795d7002fd
khaledosman <khaled.osmaan@gmail.com>
2021-02-13 13:42:35 +0100

    linting

--- src/common/api.service.ts
+++ src/common/api.service.ts
@@ -1,5 +1,5 @@
 import JwtService from "@/common/jwt.service";
-import axios from 'axios'
+import axios from "axios";
 import { API_URL } from "@/common/config";
 
 export const axiosClient = axios.create({
